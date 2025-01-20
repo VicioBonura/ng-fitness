fc3661957ce74cd6bd33e286d617d68a65d70da4
VicioBonura <vicio.bonura@gmail.com>
2025-01-20 12:29:00 +0100

    feat: init routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'courses', component: CoursesComponent},
+    {path: 'bookings', component: BookingsComponent},
+    {path: 'admin', component: AdminComponent},
+    {path: '**', redirectTo: 'not-found'}
+];
