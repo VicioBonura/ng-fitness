fd864d7fefb7eaa213576cbdfc77691ccb1cc479
VicioBonura <vicio.bonura@gmail.com>
2025-01-22 18:25:39 +0100

    feat: init api service

---
+++ src/app/api.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private httpClient = inject(HttpClient);
+  private baseUrl = 'http://localhost:3000';
+
+  get<T>(url: string, errorMessage: string) {
+    const endpoint = `${this.baseUrl}/${url}`; 
+    return this.httpClient.get<T>(endpoint)
+      .pipe(
+        catchError(error => {
+          return throwError(() => new Error(errorMessage))
+        })
+      )
+  }
+
+  getById<T>(url: string, id: string, errorMessage: string) {
+    const endpoint = `${this.baseUrl}/${url}/${id}`;
+    return this.httpClient.get<T>(endpoint)
+      .pipe(
+        catchError(error => {
+          return throwError(() => new Error(errorMessage));
+        })
+      )
+  } 
+
+  post(url: string, data: any, errorMessage: string) {
+    const endpoint = `${this.baseUrl}/${url}`;
+    return this.httpClient.post(endpoint, data)
+      .pipe(
+        catchError(error => {
+          return throwError(() => new Error(errorMessage));
+        })
+      )
+  }
+
+  delete(url: string, id: string, errorMessage: string) {
+    const endpoint = `${this.baseUrl}/${url}`;
+    return this.httpClient.delete(endpoint)
+      .pipe(
+        catchError(error => {
+          return throwError(() => new Error(errorMessage));
+        })
+      )
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,5 @@ import { FooterComponent } from './footer/footer.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app-routing';
+  title = 'NG-Fitness';
 }
